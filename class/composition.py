c75f47bf4211d8c6006ab7b9e9e065405908fd38
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-17 10:29:07 +0500

    composition_practice

--- class/composition.py
+++ class/composition.py
@@ -2,6 +2,16 @@ class student:
     def study(self):
         print("studying")
 
+class classroom:
+    def __init__(self):
+        self.ali=student()
+        print('we are going to study')
+
+
+obj=classroom()
+obj.ali.study()
+
+
 
 
 # in compositon one property use the 
