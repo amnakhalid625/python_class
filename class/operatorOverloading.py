604373a5b91c7c006fc127e6ac45eda00122da4b
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-18 12:01:50 +0500

    sign_up_form

--- class/operatorOverloading.py
+++ class/operatorOverloading.py
@@ -5,7 +5,15 @@ class OperatorOverloading:
 
     def __add__(self, other):
         
-        return OperatorOverloading(self.a+other.a,self.b,other.b) 
+        return OperatorOverloading(self.a+other.a,self.b,other.b)
+
+obj=OperatorOverloading(1,2)
+obj1=OperatorOverloading(3,4)
+print(obj+obj1) 
+
+
     
-     
+
+
+
     
