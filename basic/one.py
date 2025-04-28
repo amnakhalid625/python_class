58c7cd510894cf68d3e0d0113cc9ddf22a656da4
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-28 11:00:03 +0500

    elite_partners_project

---
+++ basic/one.py
@@ -0,0 +1,28 @@
+print('hello')
+
+grade=int(input('enter you marks'))
+
+if(grade>90):
+    print('your grade is A')
+
+elif(grade>70):
+    print('your grade is B')
+
+elif(grade>=4040):
+    print('your grade is c')
+
+else:
+   print('you are fail')
+
+# list
+
+# pop is used for the remove the last element
+
+# if we give the index in the pop method then remove the element in the list
+
+name=['one','two','three']
+name.pop(1)
+print(name)
+
+# for loop
+
