7a7fc7a569b0795ff428281039694aebaf9ca1ba
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-29 11:00:12 +0500

    pythonPractive

---
+++ class/Dictonary.py
@@ -0,0 +1,17 @@
+
+
+student={
+    "name":'aman',
+    "age":20,
+    "marks":90
+}
+
+print(student.get('name'))
+
+
+for key in student:
+    if key=='name':
+        print('key found:name')
+        break
+    else:
+        print('key not found')
