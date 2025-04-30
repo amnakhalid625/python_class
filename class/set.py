bd5050899358554d445f36c2667c1d725dbe03ed
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-04-30 11:53:11 +0500

    set

--- class/set.py
+++ class/set.py
@@ -1,8 +1,25 @@
 # what is set
 # function is set of code usd to perform the specific task
 
-def student():
-    print('this is form')
+# def student():
+#     print('this is form')
 
 
-student()
+# student()
+
+# set
+sdudent={22,33,55,66,4,4,3}
+
+print(sdudent)
+
+student =set([22,4,3,2,2,2])
+added=student.add(44)
+removed=student.remove(4)
+
+
+print(student)
+
+
+marks.discard
+
+
